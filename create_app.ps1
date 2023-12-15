aa5bb40378a9bb30428a3beea800f9307ce89f7f
somayyah <somayyahmohd95@gmail.com>
2023-12-15 20:46:56 +0300

    use flutter pub global activate rename to rename the pakcage

--- create_app.ps1
+++ create_app.ps1
@@ -2,7 +2,13 @@
 $projectName = Read-Host "Enter the name for your Flutter project"
 $projectName = $projectName.ToLower().Replace(" ", "_")
 
-# Ask for the base package name
+# Create a new Flutter project
+flutter create $projectName
+
+# Change directory to the new project
+Set-Location $projectName
+
+# Ask for the base package name with a default option
 $basePackageName = Read-Host "Enter the base package name (default: com.example)"
 if ([string]::IsNullOrWhiteSpace($basePackageName)) {
     $basePackageName = "com.example"
@@ -11,22 +17,12 @@ if ([string]::IsNullOrWhiteSpace($basePackageName)) {
 # Generate the complete package name
 $packageName = $basePackageName + "." + $projectName
 
-# Create a new Flutter project
-flutter create $projectName
-
-# Change directory to the new project
-Set-Location $projectName
+# Activate and use the rename package
+flutter pub global activate rename
+flutter pub global run rename --bundleId $packageName
 
 # Setup Firebase using flutterfire CLI
 flutterfire configure
-
-# Update AndroidManifest.xml
-$androidManifestPath = "android/app/src/main/AndroidManifest.xml"
-(Get-Content $androidManifestPath) -replace 'package="com.example.appname"', "package=`"$packageName`"" | Set-Content $androidManifestPath
-
-# Update build.gradle
-$buildGradlePath = "android/app/build.gradle"
-(Get-Content $buildGradlePath) -replace 'applicationId "com.example.appname"', "applicationId `"$packageName`"" | Set-Content $buildGradlePath
 
 # Print the package name for Firebase setup
 Write-Host "Use this package name to set up your Firebase project: $packageName"
